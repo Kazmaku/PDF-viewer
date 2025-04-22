bffa9661b171c078abbd120e63a0681bc78a9b7c
Kazmaku <74229607+Kazmaku@users.noreply.github.com>
2025-04-22 17:32:07 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 4040;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`伺服器運行於 http://localhost:${port}`);
+});
